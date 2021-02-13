fb68f2d8df268d15e4dbab62d1a6678b42a12787
jordannakamoto <jordannakamoto@gmail.com>
2021-02-12 17:01:45 -0800

    ssl

--- app.js
+++ app.js
@@ -46,15 +46,15 @@ app.use(session({
 }));
 
 //HTTPS redirect
-app.use (function (req, res, next) {
-        if (req.secure) {
-                // request was via https, so do no special handling
-                next();
-        } else {
-                // request was via http, so redirect to https
-                res.redirect('https://' + req.headers.host + req.url);
-        }
-});
+app.use (requireHTTPS);
+
+function requireHTTPS(req, res, next) {
+  // The 'x-forwarded-proto' check is for Heroku
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
+    return res.redirect('https://' + req.get('host') + req.url);
+  }
+  next();
+}
 
 // passport authentication
 require('./config/passport');
